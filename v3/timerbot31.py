8adff66e395f59e7426da6451c201d268016f55c
Yusuf_M_Thon_iD <32301831+Sunda001@users.noreply.github.com>
2024-12-14 19:01:05 +0700

    Update timerbot31.py

--- v3/timerbot31.py
+++ v3/timerbot31.py
@@ -416,7 +416,7 @@ class bot_timer():
             req = requests.get(link).json()
         except Exception as e:
             logger.error(f"[{datetime.now()}] API request failed: {str(e)}")
-            return update.message.reply_text(f"Failed to fetch media {e}\n{args}")
+            return update.message.reply_text(f"Failed to fetch media {e}\n{link}")
     
         if req['success']:
             medias = []
