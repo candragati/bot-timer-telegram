c9761883e91952fdbb516a47248c3af1942d10ff
candragati <candragati@users.noreply.github.com>
2024-12-15 19:54:19 +0700

    add import dotenv and rename config.example.py to config.py,

---
+++ v3/config.py
@@ -0,0 +1,24 @@
+import sqlite3
+from telegram.ext import Updater
+from dotenv import load_dotenv
+
+load_dotenv()
+
+db          = sqlite3.connect("database", check_same_thread = False)
+cur         = db.cursor()
+class Config():
+    TOKEN    = os.environ.get('TOKEN', None)
+    BOT_ID   = os.environ.get('BOT_ID', None) 
+    APINEWS  = os.environ.get('APINEWS', None)
+    ICETEA   = os.environ.get('ICETEA', None)    
+    updater  = Updater(TOKEN,use_context = True,workers = 8,request_kwargs = {'read_timeout':600,'connect_timeout':600})
+    dp = updater.dispatcher
+    
+    
+def eksekusi(sql):
+    cur.execute(sql)
+    lineData = cur.fetchall()
+    totData = len(lineData)
+    return lineData, totData
+
+
